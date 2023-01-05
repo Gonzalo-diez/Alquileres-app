67fd6b2a34460d03efaab2f5f775985063242253
Gonzalo <gonzalodiez97@gmail.com>
2023-01-05 11:14:23 -0300

    Arreglos en el archivo firebase

--- src/components/db/Firebase.jsx
+++ src/components/db/Firebase.jsx
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database"
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
@@ -13,6 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
-export const storage = getStorage(app)
-export const auth = getAuth(app)
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
